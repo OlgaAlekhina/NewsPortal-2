2d27eed6e2f3e9e449e3d503019dfe4691a10267
OlgaAlekhina <89858498+OlgaAlekhina@users.noreply.github.com>
2021-11-29 15:41:33 +0300

    Update custom_filters.py

--- NewsPortal/news/templatetags/custom_filters.py
+++ NewsPortal/news/templatetags/custom_filters.py
@@ -4,14 +4,14 @@ register = template.Library()
 
 @register.filter(name='censor')
 def censor(text):
-    stop_words = [['хуй', '..'], ['ебат', '...'], ['пизд', '...'], ['бляд', '...']] # и всё в таком духе
+    stop_words = ['хуй', 'ебан', 'ебат', 'хуев', 'пизд', 'бляд'] # и всё в таком духе
     
     ind = 0
 
 	for stop in stop_words:
-		while stop[0] in text:
-			ind = (text.lower()).index(stop[0])
-			text = text[0 : ind + 1] + stop[1] + text[(ind + len(stop[0])):]
+		while stop in text:
+			ind = (text.lower()).index(stop)
+			text = text[0 : ind + 1] + '...' + text[(ind + len(stop)):]
 
 	return text
 
